39339c3aa57f6a04db9b8c77acd2daa32b7e107f
Truffie <trufanov.vl2013@gmail.com>
2024-04-04 21:43:57 +0300

    feat: init client

--- index.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "dotenv";
-import App from "./src/app";
-config();
-const PORT = process.env.PORT;
-App.listen(PORT, () => {
-  console.log(`server is running on port: ${PORT}`);
-});
