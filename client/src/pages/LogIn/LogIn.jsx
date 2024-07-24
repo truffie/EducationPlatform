ab3c7f2f3bb82af1d67b83efc0eb9921d7f90fd0
Truffie <trufanov.vl2013@gmail.com>
2024-07-24 23:00:17 +0300

    feat: add log in page

--- client/src/pages/LogIn/LogIn.jsx
+++ client/src/pages/LogIn/LogIn.jsx
@@ -1,17 +1,49 @@
-//import { React } from " react "
-import styles from "./LogIn.module.css";
-import { Footer } from "../../Components/Footer/Footer";
-import { Header } from "./../../Components/Header/Header";
+import styles from './LogIn.module.css';
+
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Footer } from '../../Components/Footer/Footer';
+import { Header } from '../../Components/Header/Header';
+import { Button } from '../../Components/Button/Button';
+import { request } from '../../helper/request';
 
 export function LogIn() {
+  const refs = useRef({
+    email: null,
+    pwd: null,
+  });
+  const navigate = useNavigate();
+
+  const fields = ['email', 'pwd'];
+
+  async function getInp() {
+    const data = {};
+    for (let inp in refs.current) {
+      data[inp] = refs.current[inp].value;
+    }
+    const req = await request('api/auth', 'post', data, navigate, '/');
+    console.log(req);
+  }
+
   return (
     <>
       <Header />
-      <div className={`${"bgColored"} ${"main"}`}>
-        <div className="wrapper">
+      <div className={`bgColored main`}>
+        <div className={`wrapper ${styles.wrapper}`}>
           <div className={styles.content}>
-            <h1 className={`${"ContentHeader"} ${"mainHeader"}`}>Login</h1>
+            <h1 className={`${'ContentHeader'} ${'mainHeader'}`}>Login</h1>
+            {fields.map((field) => (
+              <input
+                key={field}
+                ref={(el) => (refs.current[field] = el)}
+                type={field === 'pwd' ? 'password' : 'text'}
+                placeholder={field}
+              />
+            ))}
+            <Button onClick={getInp} className="primaryColored" value="Login" />
           </div>
+          <div className={styles.image}></div>
         </div>
       </div>
       <Footer />
