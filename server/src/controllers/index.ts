4f3843f0abb0dba3bcebb5d0e7e064bf315afda9
Truffie <trufanov.vl2013@gmail.com>
2024-07-24 23:03:00 +0300

    chore: refactor & create index

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,4 @@
+export { default as routeUser } from "./user.controller";
+export { default as routeCourse } from "./course.controller";
+export { default as routeApi } from "./api.controller";
+export { default as routeLesson } from "./lesson.controller";
