cf646f71a0cd6cb4b0e36797290568b4bbe6f17e
Truffie <trufanov.vl2013@gmail.com>
2024-07-24 22:59:50 +0300

    refactoring

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,28 +1,34 @@
-import styles from "./Home.module.css";
-import { Header } from "../../Components/Header/Header";
-import { Footer } from "../../Components/Footer/Footer";
-import { Link } from "react-router-dom";
-import { Button } from './../../Components/Button/Button';
+import styles from './Home.module.css';
+
+import { Footer } from '../../Components/Footer/Footer';
+import { Button } from '../../Components/Button/Button';
+import { Header } from '../../Components/Header/Header';
+
 function Home() {
   return (
     <>
       <Header />
-<main className="main">
-  
-        <section className={`${"bgColored"} ${styles.about}`}>
-          <div className={`${styles.sectionWrapper} ${"wrapper"}`}>
-            <div className={styles.Content}>
+      <main className="main">
+        <section className={`bgColored ${styles.about}`}>
+          <div className={`${styles.sectionWrapper} wrapper`}>
+            <div>
               <p className={styles.primaryColored}>E-COURSE PLATFORM</p>
-              <div className={styles.ContentText}>
+              <div className={styles.Content}>
                 <h1 className={`${styles.ContentHeader} ${styles.mainHeader}`}>
                   Learning and teaching online, made easy.
                 </h1>
-                <p className={styles.ContentSubtitle}>Any subject, in any language, on any device, for all ages!</p>
-              </div>
-              <Button className={styles.primaryColored} value={"About platform"} path="/course"/>
-              <div className={styles.statsWrapper}>
-                <p className={`${styles.stats}`}>600</p>
-                <p className={styles.StatsSubtitle}>Students</p>
+                <p className={styles.ContentSubtitle}>
+                  Any subject, in any language, on any device, for all ages!
+                </p>
+                <Button
+                  className={`${styles.primaryColored}`}
+                  value={'About platform'}
+                  path="/course"
+                />
+                <div className={styles.statsWrapper}>
+                  <p className={`${styles.stats}`}>600</p>
+                  <p className={styles.StatsSubtitle}>Students</p>
+                </div>
               </div>
             </div>
             <div className={`${styles.aboutImg}`}>
@@ -31,45 +37,61 @@ function Home() {
           </div>
         </section>
         <section className={styles.learn}>
-          <div className={`${styles.sectionWrapper} ${"wrapper"}`}>
+          <div className={`${styles.sectionWrapper} wrapper`}>
             <div className={`${styles.learnImg}`}></div>
             <div className={styles.Content}>
-              <div className={styles.ContentText}>
-                <h2 className={styles.ContentHeader}>Learn a language in a playful way</h2>
-                <p className={styles.ContentSubtitle}>Make learning programming languages more fun with mini-games</p>
-              </div>
+              <h2 className={styles.ContentHeader}>
+                Learn a language in a playful way
+              </h2>
+              <p className={styles.ContentSubtitle}>
+                Make learning programming languages more fun with mini-games
+              </p>
               <div className={styles.buttonsWrapper}>
-                <div className={`${styles.secondaryColored} ${styles.button} ${styles.customButton}`}>Sprint →</div>
-                <div className={`${styles.button} ${styles.primaryColored} ${styles.customButton}`}>Tasks →</div>
+                <Button
+                  className={`${styles.secondaryColored} ${styles.button} ${styles.customButton}`}
+                  value="Sprint →"
+                />
+                <Button
+                  className={`${styles.button} ${styles.primaryColored} ${styles.customButton}`}
+                  value="Tasks →"
+                />
               </div>
             </div>
           </div>
         </section>
-        <section className={`${styles.textbook} ${"bgColored"}`}>
-          <div className={` ${styles.sectionWrapper} ${"wrapper"}`}>
+        <section className={`${styles.textbook} bgColored`}>
+          <div className={` ${styles.sectionWrapper} wrapper`}>
             <div className={styles.Content}>
-              <div className={styles.ContentText}>
-                <h2 className={styles.ContentHeader}>Increase your knowledge</h2>
-                <p className={styles.ContentSubtitle}>Traditional and new effective approaches to learning languages</p>
-              </div>
-              <button className={`${styles.button} ${styles.primaryColored}`}>Textbook →</button>
+              <h2 className={styles.ContentHeader}>Increase your knowledge</h2>
+              <p className={styles.ContentSubtitle}>
+                Traditional and new effective approaches to learning languages
+              </p>
+              <Button
+                className={`${styles.primaryColored}`}
+                value="Textbook →"
+              />
             </div>
             <div className={styles.textbookImg}></div>
           </div>
         </section>
         <section className={styles.statistic}>
-          <div className={` ${styles.sectionWrapper} ${"wrapper"}`}>
+          <div className={` ${styles.sectionWrapper} wrapper`}>
             <div className={styles.statisticImg}></div>
             <div className={styles.Content}>
-              <div className={styles.ContentText}>
-                <h2 className={styles.ContentHeader}>Watch your progress every day</h2>
-                <p className={styles.ContentSubtitle}>Save statistics on your achievements and mistakes</p>
-              </div>
-              <button className={`${styles.button} ${styles.primaryColored}`}>Statistics →</button>
+              <h2 className={styles.ContentHeader}>
+                Watch your progress every day
+              </h2>
+              <p className={styles.ContentSubtitle}>
+                Save statistics on your achievements and mistakes
+              </p>
+              <Button
+                className={`${styles.primaryColored}`}
+                value="Statistics →"
+              />
             </div>
           </div>
         </section>
-</main>
+      </main>
       <Footer />
     </>
   );
