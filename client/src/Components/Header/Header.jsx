d7d450687151d86828d4ded55273a3f285c2d8e4
Truffie <trufanov.vl2013@gmail.com>
2024-07-24 22:58:39 +0300

    feat: add sign out button

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,17 +1,58 @@
-//import { React } from " react "
-import styles from "./Header.module.css";
-import { Button } from "./../Button/Button";
-import { Link } from "react-router-dom";
+import styles from './Header.module.css';
+
+import { React, useEffect, useState } from 'react';
+
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from '../Button/Button';
+
+import Cookie from 'js-cookie';
+
 export function Header() {
+  const [token, setToken] = useState('');
+  const navigate = useNavigate();
+
+  function getToken() {
+    const aplicationToken = Cookie.get('accessToken');
+    setToken(aplicationToken);
+  }
+
+  useEffect(() => getToken(), [token]);
+
+  function deleteToken() {
+    Cookie.remove('accessToken');
+    setToken('');
+    navigate('/');
+  }
+
   return (
-    <header className={`${"bgColored"} ${"lineAccent"} ${styles.header}`}>
-      <div className={`${styles.headerWrapper} ${"wrapper"} ${"layout2Columns"}`}>
+    <header className={`${'bgColored'} ${'lineAccent'} ${styles.header}`}>
+      <div
+        className={`${styles.headerWrapper} ${'wrapper'} ${'layout2Columns'}`}
+      >
         <Link to="/">
-          <p className={styles.logo}>Hschool</p>
+          <p className={`${styles.logo} animationHovered`}>Hschool</p>
         </Link>
         <div>
-            <Button className={styles.login} value={"Login →"} path={"/login"}/>
-            <Button className={styles.signup} value={"Sign Up"}  path={"/signup"}/>
+          {token ? (
+            <Button
+              onClick={deleteToken}
+              className={styles.login}
+              value={'Log out'}
+            ></Button>
+          ) : (
+            <>
+              <Button
+                className={styles.login}
+                value={'Login →'}
+                path={'/login'}
+              />
+              <Button
+                className={styles.signup}
+                value={'Sign Up'}
+                path={'/signup'}
+              />
+            </>
+          )}
         </div>
       </div>
     </header>
