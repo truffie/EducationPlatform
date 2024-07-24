1426d244931e8a86eca90a2ef1e7a935e9c18786
Truffie <trufanov.vl2013@gmail.com>
2024-07-24 22:57:54 +0300

    refactoring

--- client/src/Components/Button/Button.jsx
+++ client/src/Components/Button/Button.jsx
@@ -1,12 +1,12 @@
-//import { React } from " react "
 import { Link } from "react-router-dom";
 import styles from "./Button.module.css";
-export function Button({ value, className, path }) {
-  console.log(className);
+export function Button({ value, className, path, onClick }) {
   return (
     <>
       <Link to={path}>
-        <button className={`${styles.button} ${className || ""}`}>{value}</button>
+        <button className={`${styles.button} animationHovered ${className || ""}`} onClick={onClick}>
+          {value}
+        </button>
       </Link>
     </>
   );
